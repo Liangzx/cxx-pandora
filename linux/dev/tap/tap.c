52debc69f865cbce9c341e740f68addc5bfcdfa6
liangzhixu <liangzhixu@scutech.com>
2025-07-29 15:14:59 +0800

    feat: add dev

---
+++ linux/dev/tap/tap.c
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 硬货 | 如何用 tap/tun 设备编写一个 ICMP 程序
+// https://mp.weixin.qq.com/s?__biz=MzI1OTY2MzMxOQ==&mid=2247484981&idx=1&sn=6c77186291b17a855d790ee8e5722c25&poc_token=HG1piGijfVhu6ilObfwVLo4VuAZ52t1SjRbgzolg
