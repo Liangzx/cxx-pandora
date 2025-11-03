4a24c627c195b4d3fa51238e82a8383bb2788000
liangzhixu <liangzhixu@scutech.com>
2025-11-03 10:31:18 +0800

    fix: modify in_place

--- idioms/in_place.cpp
+++ idioms/in_place.cpp
@@ -8,7 +8,7 @@ void *operator new(std::size_t count) {
 }
 
 void operator delete(void *ptr) noexcept {
-  std::cout << "global op delete called\n";
+  std::cout << "global op delete called" << std::endl;
   std::free(ptr);
 }
 
@@ -66,5 +66,11 @@ private:
 int main() {
   // std::optional<UserName> opt(UserName{});
   // std::optional<UserName> opt({});
+  // 没有调用 operator new​​：在使用 std::in_place构造 std::optional时（第1部分），
+  // 输出显示直接调用了 MyClass的构造函数，但​​没有调用​​我们重载的 MyClass::operator new。
+  // 这强烈表明 std::optional在内部已经预先管理好了内存（就像我们预先分配的 buffer），
+  // 然后直接在该内存上构造对象，这正是 placement new 的核心思想
   std::optional<UserName> opt{std::in_place};
 }
+
+// https://mp.weixin.qq.com/s?__biz=MzU0OTkzNjU2OA==&mid=2247486464&idx=1&sn=3801f746d605215171e58574abbd6384&poc_token=HHV5BGmjQ_phWIa-8WjVCNdKHzetV5PZM_w-y8LL
